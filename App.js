257a88d8062d0f3d268a19271d75cc2b0946c6b9
LucassantoSCruz <lucas.cruz162@etec.sp.gov.br>
2023-04-01 12:52:39 -0300

    Criação FrontEnd

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+
+  const [pesquisa, setPesquisa] = useState([]);
+
+  const requestResponse = () => {
+    axios.get('http://10.0.2.2:3001/listarCategoria')
+
+      .then(function (response) {
+        setPesquisa(response.data)
+        console.log("RESULTADO DA BUSCA: ", pesquisa)
+
+      })
+
+      .catch(function (error) {
+        console.log(error)
+
+      })
+  }
+
+  useEffect(() => {
+    requestResponse()
+  },[])
+
+  const Caixa = ({campo}) => (
+    <View style={styles.Caixa}>
+      <Text style={styles.title}>{campo}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text>
+        Listagem Axios
+      </Text>
+
+      <FlatList
+        data={pesquisa.data}
+        renderItem={({ item }) => <Caixa campo={item.nome_login}/>}
+        keyExtractor={item => item.cod_login} 
+      />
+
     </View>
   );
 }
@@ -16,5 +54,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  Caixa: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
